6f50d2b1abd444104d4cff96fc05fa050618ae76
Don Samson <donboscosamson@gmail.com>
2017-11-08 00:16:16 -0500

    Fixed build issues

--- src/test/java/samplepageobj/test/FunctionalTest.java
+++ src/test/java/samplepageobj/test/FunctionalTest.java
@@ -15,7 +15,7 @@ public class FunctionalTest {
 	
 	@BeforeClass
 	public static void setUp(){
-		System.setProperty("webdriver.gecko.driver", "C:\\Don\\Jars\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\workspace\\pageObjectSamplePrj\\drivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
